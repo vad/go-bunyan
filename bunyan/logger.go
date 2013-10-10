f0eef4157f78014e40327598d2509af5d4dcf790
Davide Setti <davide.setti@gmail.com>
2013-10-10 10:42:03 +0200

    added Logm family functions (Debug, Infom, ...) that logs additional data

--- bunyan/logger.go
+++ bunyan/logger.go
@@ -35,8 +35,10 @@ func NewLogger(name string) Logger {
 	return Logger{Name: name, logger: logger}
 }
 
-func (logger *Logger) getBaseData() map[string]interface{} {
-	data := make(map[string]interface{})
+func (logger *Logger) getBaseData(data map[string]interface{}) map[string]interface{} {
+	if data == nil {
+		data = make(map[string]interface{})
+	}
 	data["name"] = logger.Name
 	data["pid"] = pid
 	hostname, err := os.Hostname()
@@ -48,9 +50,19 @@ func (logger *Logger) getBaseData() map[string]interface{} {
 }
 
 func (l *Logger) Log(level int, v ...interface{}) error {
-	data := l.getBaseData()
+	data := l.getBaseData(nil)
 	data["msg"] = fmt.Sprint(v...)
-	data["level"] = 30
+	data["level"] = level
+	out, _ := json.Marshal(data)
+	l.logger.Println(string(out))
+
+	return nil
+}
+
+func (l *Logger) Logm(level int, s string, m map[string]interface{}) error {
+	data := l.getBaseData(m)
+	data["msg"] = s
+	data["level"] = level
 	out, _ := json.Marshal(data)
 	l.logger.Println(string(out))
 
@@ -67,32 +79,16 @@ func (l *Logger) Debug(v ...interface{}) error {
 	return l.Log(LOG_DEBUG, v...)
 }
 
-func (l *Logger) Debugf(s string, v ...interface{}) error {
-	return l.Log(LOG_DEBUG, fmt.Sprintf(s, v...))
-}
-
 func (l *Logger) Info(v ...interface{}) error {
 	return l.Log(LOG_INFO, v...)
-}
-
-func (l *Logger) Infof(s string, v ...interface{}) error {
-	return l.Log(LOG_INFO, fmt.Sprintf(s, v...))
 }
 
 func (l *Logger) Warning(v ...interface{}) error {
 	return l.Log(LOG_WARNING, v...)
 }
 
-func (l *Logger) Warningf(s string, v ...interface{}) error {
-	return l.Log(LOG_WARNING, fmt.Sprintf(s, v...))
-}
-
 func (l *Logger) Err(v ...interface{}) error {
 	return l.Log(LOG_ERR, v...)
-}
-
-func (l *Logger) Errf(s string, v ...interface{}) error {
-	return l.Log(LOG_ERR, fmt.Sprintf(s, v...))
 }
 
 // FATAL
@@ -102,8 +98,46 @@ func (l *Logger) Fatal(v ...interface{}) (err error) {
 	return
 }
 
-func (l *Logger) Fatalf(s string, v ...interface{}) (err error) {
-	err = l.Log(LOG_FATAL, fmt.Sprintf(s, v...))
+func (l *Logger) Debugf(s string, v ...interface{}) error {
+	return l.Log(LOG_DEBUG, fmt.Sprintf(s, v...))
+}
+
+func (l *Logger) Infof(s string, v ...interface{}) error {
+	return l.Log(LOG_INFO, fmt.Sprintf(s, v...))
+}
+
+func (l *Logger) Warningf(s string, v ...interface{}) error {
+	return l.Log(LOG_WARNING, fmt.Sprintf(s, v...))
+}
+
+func (l *Logger) Errf(s string, v ...interface{}) error {
+	return l.Log(LOG_ERR, fmt.Sprintf(s, v...))
+}
+
+func (l *Logger) Fatalf(s string, v ...interface{}) error {
+	l.Log(LOG_FATAL, fmt.Sprintf(s, v...))
 	os.Exit(1)
-	return
+	return nil
+}
+
+func (l *Logger) Debugm(s string, m map[string]interface{}) error {
+	return l.Logm(LOG_DEBUG, s, m)
+}
+
+func (l *Logger) Infom(s string, m map[string]interface{}) error {
+	return l.Logm(LOG_INFO, s, m)
+}
+
+func (l *Logger) Warningm(s string, m map[string]interface{}) error {
+	return l.Logm(LOG_WARNING, s, m)
+}
+
+func (l *Logger) Errm(s string, m map[string]interface{}) error {
+	return l.Logm(LOG_ERR, s, m)
+}
+
+func (l *Logger) Fatalm(s string, m map[string]interface{}) error {
+	l.Logm(LOG_FATAL, s, m)
+	os.Exit(1)
+	return nil
 }
